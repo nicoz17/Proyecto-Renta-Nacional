fb7b7f50d273341d7d188ec7fe7c0a1129ba0194
crisalcaino99 <cjalcaino@uc.cl>
2025-04-04 11:47:10 -0300

    zapit weco

---
+++ datacleaner.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def cleanmenosuno(file, columna):
+    # limpia los menos uno de alguna columna
+    df = pd.read_excel(file, engine = 'openpyxl')
+    df_filtrado = df[df[columna] != -1]
+    output = file[:-4]+'output'
+    df_filtrado.to_csv(output, index = False) 
+    return None
+
+# la gracia seria hacer un main que limpia los datos a nuestro gusto
