2cbafaffa3b6244f3cfa076502eca210a70030d3
crisalcaino99 <cjalcaino@uc.cl>
2025-04-04 12:06:30 -0300

    Commit1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Proyecto-Renta-Nacional
 Predicción de Ventas de Rentas Vitalicias Según Cotizaciones y Ranking Regulatorio
+
+Incluido en el gitignore las exclusiones de los archivos .csv y .xlsx
--- datacleaner.py
+++ datacleaner.py
@@ -1,12 +1,25 @@
 import pandas as pd
 
-def cleanmenosuno(file, columna):
+# columnas -> lista de strings
+def cleanmenosuno(file, columnas):
     # limpia los menos uno de alguna columna
     df = pd.read_excel(file, engine = 'openpyxl')
-    df_filtrado = df[df[columna] != -1]
-    output = file[:-4]+'output'
-    df_filtrado.to_csv(output, index = False) 
-    return None
+    for col in columnas:
+        if col in df.columns:
+            df = df[df[col] != -1]
+        else:
+            print("no esta la columna en el archivo")
+            return None
+    
+    return df
 
 # la gracia seria hacer un main que limpia los datos a nuestro gusto
 #######
+def main():
+    archivo = 'FILE'
+    columnas_a_limpiar = ['col1', 'col2']
+    df = cleanmenosuno(archivo, columnas_a_limpiar)
+    
+    output = archivo.replace('.xlsx', '_output.csv')
+    df.to_csv(output, index=False)
+    return 1
